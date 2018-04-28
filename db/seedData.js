ebf45e649765d86a1c7e3a5aa63ea7a096c3034d
Dameon1 <dameonmedoza@gmail.com>
2018-04-27 19:34:59 -0700

    e

--- db/seedData.js
+++ db/seedData.js
@@ -4,7 +4,7 @@ let util = require('util');
 let exec = util.promisify(require('child_process').exec);
 
 module.exports = function(file){
-    return exec(`psql -f ${file} -d postgres://postgres:@localhost/noteful-test`);
+    return exec(`psql -f ${file} -d postgres://dev:dev@localhost/noteful-test`);
 };
 
 
